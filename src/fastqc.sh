8d05632a40ee6830b2fb19dc49cabce8b1755931
jholy <jholy@gmail.com>
2019-11-06 10:32:42 +0000

    code fastqc.sh

--- src/fastqc.sh
+++ src/fastqc.sh
@@ -1,4 +1,14 @@
 #! /bin/bash
+resultfastqc=/home/rstudio/disk/resultfastqc
+#variable contenant le chemin de fichiers  
+FASTQ=`ls /home/rstudio/disk/sra_data` 
+# ls: list guillaumés inversés 
+# FASTQ envoie une liste de noms des infos sur Sra_data
+mkdir -p $resultfastqc
+#mk dir permet de creer le dossier "resultfastqc" pour le stockage des fichiers issus de FASTQ 
+for fichier in $FASTQ 
+do 
+fastqc /home/rstudio/disk/sra_data/$fichier -o $resultfastqc
+# envoie les fichiers issues de la fonction fastqc sur resultfastqc 
+done 
 
-
-fastqc -h
