797820ea30610ebd173c3e86ddf90caf1bd0f94f
jholy <jholy@gmail.com>
2019-11-06 10:56:47 +0000

    fichier multiqc

--- src/multiqc.sh
+++ src/multiqc.sh
@@ -1,4 +1,10 @@
 #! /bin/bash
-multiqc
+multiqcresult=/home/rstudio/disk/multiqresult
+resultfastqc=/home/rstudio/disk/resultfastqc
+mkdir -p $multiqcresult
+cd $multiqcresult
+# cd est necessaire pour placer l'endroit auquel multiqc doit agir
+multiqc $resultfastqc/*
+#*: tous les fichiers du dossier
 
 
